ee9feab6a7e45ac8e3e0a01e42b0b17a0adb9398
falyap <57078343+falyap@users.noreply.github.com>
2020-05-13 00:20:57 -0700

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,6 +1,5 @@
 class profile::agent_nodes {
-  include docker
-#  include dockeragent
-#  dockeragent::node {'web.puppet.vm':}
-#  dockeragent::node {'db.puppet.vm':}
+  include dockeragent
+  dockeragent::node {'web.puppet.vm':}
+  dockeragent::node {'db.puppet.vm':}
 }
