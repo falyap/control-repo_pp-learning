31025a1262e8b701529d8d934782899d1faeeb34
falyap <57078343+falyap@users.noreply.github.com>
2020-05-24 22:41:26 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -19,8 +19,11 @@ class minecraft (
     require => File['/opt/minecraft/minecraft_server.jar'],
   }
   file { '/etc/systemd/system/minecraft.service':
-    ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    ensure  => file,
+#    source => 'puppet:///modules/minecraft/minecraft.service',
+    content => epp('minecraft/minecraft.service', {
+      install_dir => $install_dir,
+    }),
   }
   service { 'minecraft':
     ensure  => running,
