bedf7c9ec631765fadfaec5d696f52de5bf987ab
falyap <57078343+falyap@users.noreply.github.com>
2020-05-24 23:03:42 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,5 +1,5 @@
 class minecraft (
-#  $url = 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+#  $url = 'puppet:///modules/minecraft/minecraft_server.jar',
   $install_dir = '/opt/minecraft',
   ) {
   file { $install_dir:
@@ -7,8 +7,7 @@ class minecraft (
   }
   file { "${install_dir}/minecraft_server.jar":
     ensure  => file,
-#    source  => "${url}",
-    source  => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+    source  => $url,
     require => File[$install_dir],
   }
   package { 'java':
