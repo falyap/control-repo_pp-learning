5d65805b817f5e909df6e9ea2ef9e474992a4727
falyap <57078343+falyap@users.noreply.github.com>
2020-05-24 22:18:34 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -3,8 +3,9 @@ class minecraft {
     ensure => directory,
   }
   file { '/opt/minecraft/minecraft_server.jar':
-    ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+    ensure  => file,
+    source  => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+    require => File['/opt/minecraft'],
   }
   package { 'java':
     ensure => installed,
@@ -12,13 +13,15 @@ class minecraft {
   file { '/opt/minecraft/eula.txt':
     ensure  => file,
     content => 'eula=true',
+    require => File['/opt/minecraft/minecraft_server.jar'],
   }
   file { '/etc/systemd/system/minecraft.service':
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
   service { 'minecraft':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
+    require => [File['/opt/minecraft/minecraft_server.jar'], Package['java'], File['/opt/minecraft/eula.txt'], File['/etc/systemd/system/minecraft.service']],
   }
 }
