4bef48e909ad30e6a87d64efcb8b4b4c7ff8cf31
falyap <57078343+falyap@users.noreply.github.com>
2020-05-24 22:27:08 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,16 +1,18 @@
 class minecraft {
-  file { '/opt/minecraft':
+  $url = 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar'
+  $install_dir = '/opt/minecraft'
+  file { $install_dir:
     ensure => directory,
   }
-  file { '/opt/minecraft/minecraft_server.jar':
+  file { "${install_dir}/minecraft_server.jar":
     ensure  => file,
-    source  => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+    source  => $url,
     require => File['/opt/minecraft'],
   }
   package { 'java':
     ensure => installed,
   }
-  file { '/opt/minecraft/eula.txt':
+  file { "${install_dir}/eula.txt":
     ensure  => file,
     content => 'eula=true',
     require => File['/opt/minecraft/minecraft_server.jar'],
@@ -22,6 +24,6 @@ class minecraft {
   service { 'minecraft':
     ensure  => running,
     enable  => true,
-    require => [File['/opt/minecraft/minecraft_server.jar'], Package['java'], File['/opt/minecraft/eula.txt'], File['/etc/systemd/system/minecraft.service']],
+    require => [File["${install_dir}/minecraft_server.jar"], Package['java'], File["${install_dir}/eula.txt"], File['/etc/systemd/system/minecraft.service']],
   }
 }
